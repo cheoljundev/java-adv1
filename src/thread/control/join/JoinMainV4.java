dfc934a5342b27615e4a4a978a766a1299affe68
cheoljundev <cheoljundev@gmail.com>
2024-11-29 17:41:52 +0900

    join - 특정 시간 만큼만 대기

---
+++ src/thread/control/join/JoinMainV4.java
@@ -0,0 +1,49 @@
+package thread.control.join;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class JoinMainV4 {
+    public static void main(String[] args) throws InterruptedException {
+        log("Start");
+        SumTask task1 = new SumTask(1, 50);
+        SumTask task2 = new SumTask(51, 100);
+
+        Thread thread1 = new Thread(task1, "Thread-1");
+        Thread thread2 = new Thread(task2, "Thread-2");
+
+        thread1.start();
+        thread2.start();
+
+        // 스레드가 종료될 때까지 대기
+        log("main 스레드가 thread1 종료까지 1초 기다림");
+        thread1.join(1000);
+        log("main 스레드 대기 완료");
+
+        System.out.println("task1.result + task2.result = sumAll : " + (task1.result + task2.result));
+        log("End");
+    }
+
+    static class SumTask implements Runnable {
+        int startValue;
+        int endValue;
+        int result;
+
+        public SumTask(int startValue, int endValue) {
+            this.startValue = startValue;
+            this.endValue = endValue;
+        }
+
+        @Override
+        public void run() {
+            log("작업 시작");
+            sleep(2000);
+            int sum = 0;
+            for (int i = startValue; i <= endValue; i++) {
+                sum += i;
+            }
+            result = sum;
+            log("작업 완료 result = " + result);
+        }
+    }
+}
